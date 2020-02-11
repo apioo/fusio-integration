c2f6e20ceeac0163f672c15f69784f9b78d47736
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-02-11 21:12:47 +0100

    output exception message

--- marketplace_check.php
+++ marketplace_check.php
@@ -8,18 +8,22 @@ $httpClient = new \GuzzleHttp\Client(['verify' => false]);
 foreach ($apps as $name => $app) {
     echo 'Check app ' . $name . "\n";
 
-    \Webmozart\Assert\Assert::same(1, version_compare($app['version'], '0.0'), $name);
-    \Webmozart\Assert\Assert::notEmpty($app['description'], $name);
-    \Webmozart\Assert\Assert::eq($app['screenshot'], filter_var($app['screenshot'], FILTER_VALIDATE_URL), $name);
-    \Webmozart\Assert\Assert::eq($app['website'], filter_var($app['website'], FILTER_VALIDATE_URL), $name);
-    \Webmozart\Assert\Assert::eq($app['downloadUrl'], filter_var($app['downloadUrl'], FILTER_VALIDATE_URL), $name);
+    try {
+        \Webmozart\Assert\Assert::same(1, version_compare($app['version'], '0.0'));
+        \Webmozart\Assert\Assert::notEmpty($app['description']);
+        \Webmozart\Assert\Assert::eq($app['screenshot'], filter_var($app['screenshot'], FILTER_VALIDATE_URL));
+        \Webmozart\Assert\Assert::eq($app['website'], filter_var($app['website'], FILTER_VALIDATE_URL));
+        \Webmozart\Assert\Assert::eq($app['downloadUrl'], filter_var($app['downloadUrl'], FILTER_VALIDATE_URL));
 
-    $file = __DIR__ . '/' . $name . '.zip';
-    $handle = fopen($file, 'w');
+        $file = __DIR__ . '/' . $name . '.zip';
+        $handle = fopen($file, 'w');
 
-    $response = $httpClient->get($app['downloadUrl'], ['sink' => $handle]);
+        $response = $httpClient->get($app['downloadUrl'], ['sink' => $handle]);
 
-    \Webmozart\Assert\Assert::eq(200, $response->getStatusCode());
-    \Webmozart\Assert\Assert::eq($app['sha1Hash'], sha1_file($file), $name);
-    \Webmozart\Assert\Assert::true((new \ZipArchive())->open($file, \ZipArchive::CHECKCONS));
+        \Webmozart\Assert\Assert::eq(200, $response->getStatusCode());
+        \Webmozart\Assert\Assert::eq($app['sha1Hash'], sha1_file($file));
+        \Webmozart\Assert\Assert::true((new \ZipArchive())->open($file, \ZipArchive::CHECKCONS));
+    } catch (\InvalidArgumentException $e) {
+        echo $e->getMessage() . "\n";
+    }
 }
