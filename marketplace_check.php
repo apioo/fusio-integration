3d68e7a57ce20f6993b6240707c293e31f158ed6
Christoph Kappestein <christoph.kappestein@gmail.com>
2020-02-11 20:52:19 +0100

    output app name

--- marketplace_check.php
+++ marketplace_check.php
@@ -6,6 +6,8 @@ $apps = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . '/master
 $httpClient = new \GuzzleHttp\Client(['verify' => false]);
 
 foreach ($apps as $name => $app) {
+    echo 'Check app ' . $name . "\n";
+
     \Webmozart\Assert\Assert::same(1, version_compare($app['version'], '0.0'), $name);
     \Webmozart\Assert\Assert::notEmpty($app['description'], $name);
     \Webmozart\Assert\Assert::eq($app['screenshot'], filter_var($app['screenshot'], FILTER_VALIDATE_URL), $name);
