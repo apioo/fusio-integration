45c942fa2f48cb8fa02a60d62fe9bcf0bf7668de
Christoph Kappestein <christoph.kappestein@gmail.com>
2022-04-03 10:31:04 +0200

    start from 3.0

--- run_install.php
+++ run_install.php
@@ -6,7 +6,7 @@ const DATABASE = 'fusio';
 const DATABASE_MASTER = 'fusio_master';
 
 $releases = fetchReleases();
-$releases = getReleases($releases, 'v2.0.0');
+$releases = getReleases($releases, 'v3.0.0');
 $releases = array_reverse($releases);
 
 foreach ($releases as $release) {
